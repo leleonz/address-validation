94f25ca5f08fafc5c5e22a39e7ab65cf3db67185
leleonz <leonzcyl@gmail.com>
2023-12-16 16:23:11 +0800

    initial commit

---
+++ test/AddressValidation.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace AddressValidation.IntegrationTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/AddressValidation.UnitTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace AddressValidation.UnitTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
